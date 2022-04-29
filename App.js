6348e62dab9319192781b7553b64374f974dd42d
HarshithaMWikiance <103238592+HarshithaMWikiance@users.noreply.github.com>
2022-04-29 15:02:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,81 @@
+import  React from 'react';
+import './App.css';
+
+
+function App() {
+  return(
+    <div className='App'>
+      <div style={{display:"flex",flexDirection:"row",columnGap:"40px"}}>
+      <div class="card">
+  <div class="card-image"></div>
+  <div class="card-text">
+    <h3>Id:1</h3>
+    <h3>Name:Leanne Graham</h3>
+    <p>UserName:Bret</p>
+    <p>Email:Sincere@april.biz</p>
+    <p>Address:
+      street: Kulas Light
+      city: Gwenborough,
+      zipcode: 92998-3874</p>
+    <p>Phone:1-770-736-8031 x56442</p> 
+    <p>website: hildegard.org</p>
+  </div>
+  </div>
+
+      <div class="card">
+  <div class="card-image"></div>
+  <div class="card-text">
+    <h3>Id:2</h3>
+    <h3>Name:Ervin Howell</h3>
+    <p>UserName:Antonette</p>
+    <p>Email:Shanna@melissa.tv</p>
+    <p>Address:
+      street:Victor Plains
+      city: Wisokyburgh
+      zipcode:90566-7771</p>
+    <p>Phone:010-692-6593 x09125</p> 
+    <p>website:anastasia.net</p>
+  </div>
+  </div>
+
+  <div class="card">
+  <div class="card-image"></div>
+  <div class="card-text">
+    <h3>Id:3</h3>
+    <h3>Name:Clementine Bauch</h3>
+    <p>UserName:Samantha</p>
+    <p>Email:Nathan@yesenia.net</p>
+    <p>Address:
+      street: Douglas Extension
+      city: McKenziehaven
+      zipcode: 59590-4157</p>
+    <p>Phone:1-463-123-4447</p> 
+    <p>website:ramiro.info</p>
+  </div>
+  </div>
+
+  <div class="card">
+  <div class="card-image"></div>
+  <div class="card-text">
+    <h3>Id:4</h3>
+    <h3>Name:Patricia Lebsack</h3>
+    <p>UserName:Karianne</p>
+    <p>Email:Julianne.OConner@kory.org</p>
+    <p>Address:
+      street: Hoeger Mall
+      city: South Elvis
+      zipcode: 53919-4257</p>
+    <p>Phone:493-170-9623 x156</p> 
+    <p>website:kale.biz</p>
+  </div>
+  </div>
+ 
+ 
+</div>
+</div>
+
+    
+    );
+  }
+
+  export default App;
